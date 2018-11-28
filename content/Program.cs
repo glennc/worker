afdb0dd258f558cf9fbdeabb18e1e76f1c2b23e8
Glenn Condron <glennc@microsoft.com>
2018-11-27 21:19:13 -0800

    initial

---
+++ content/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+#if WebJobs
+using Microsoft.Azure.WebJobs.Extensions.Timers;
+using Microsoft.Azure.WebJobs.Host;
+#endif
+
+namespace workerproto
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+#if WebJobs
+                .ConfigureWebJobs(webJobsBuilder => webJobsBuilder.AddTimers())
+                .ConfigureServices(services => services.AddSingleton<ScheduleMonitor, FileSystemScheduleMonitor>());
+#endif
+#if Empty
+                .ConfigureServices(services => services.AddHostedService<Worker>());
+#endif
+    }
+}
